8743b0939a0f20b463f7af8fa06707de68e2da22
Miguel Flores <miguelfloresroch@gmail.com>
2015-12-09 11:57:53 -0700

    unidades falta el croll

---
+++ Teachers Helper for ITC/RealAndProgrammedDatesByGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  RealAndProgrammedDatesByGroupViewController.h
+//  Teachers Helper for ITC
+//
+//  Created by Jesus Flores on 09/12/15.
+//  Copyright © 2015 TecDeCuliacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RealAndProgrammedDatesByGroupViewController : UIViewController
+
+@end
